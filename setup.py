5a2b9217a992a15760cc7570cce1f3b5ae597b18
EliMor <sergf@sergios-air.attlocal.net>
2014-10-21 20:27:11 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+version = "1.0.0"
+
+setup(name="googleplayapi",
+      version=version,
+      author="Emilien Girault",
+      author_email="emilien.girault@gmail.com",
+      license="BSD",
+      description="Google Play API for Android",
+      url="https://github.com/EliMor/googleplay-api",
+      py_modules=[
+        'apishell',
+        'categories',
+        'config',
+        'download',
+        'googleplay',
+        'googleplay_pb2',
+        'helpers',
+        'list',
+        'permissions',
+        'search'
+      ]
+)
